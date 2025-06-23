85297cc475b46bd9e2df55469d7415aede5a21d3
noelztorres <noelztorresjr@gmail.com>
2025-06-23 19:17:35 +0800

    Moved Files into User Side

---
+++ User Side/News & Updates/News Page.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News & Updates | Lucena</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="Navigation Bar/navigation.css">
+    <link rel="stylesheet" href="news_styles.css">
+    <link rel="stylesheet" href="Footer/footer_styles.css">
+
+</head>
+<body>
+     <header>
+        <div id="divLogo">
+            <img src="../Navigation Bar/img/lucena_city.png" class="logo"> 
+            <h1 id="cityName"> CITY OF LUCENA </h1>
+        </div>
+        <div class="tagline">
+                <img src="../Navigation Bar/img/boom-lucena.PNG" class="boomLucena">
+        </div>
+    </header>
+
+<?php
+include 'navigation.php';
+?>
+
+<section class="headline_section">
+    <div class="headline_content">
+        <div class="navigation_links">
+            <a href="#" class="home_link">Home</a> > <a class="news-link" href="#">News & Updates</a>
+        </div>
+        <h1>2025 PHALGA <p>Excellence Award</p>
+        </h1>
+        <p>The City Government of Lucena has once again received the 2025 PHALGA Excellence Award, thanks to the
+            outstanding work of our City Accounting Office led by Ms. Ella Michelle T. Azagra.
+        </p>
+        <a href="https://www.facebook.com/share/p/16hHfxQPAL/" class="readmore_link" target="_blank">Read More</a>
+    </div>
+</section>
+
+<div class="bottom-container">
+
+    <section class="news_section">
+        <h2>LATEST NEWS</h2>
+        <hr>
+        <div id="news_container">
+            <article class="news-article">
+                <img src="img/news1.jpg" alt="news1">
+                <div class="news-content">
+                    <h3 class="news-title">
+                        <span class="title-text">Lucena West 1 shines in Jakarta! </span>
+                        <span class="date">| May 26, 2025</span>
+                    </h3>
+                    <p class="summary">In Jakarta, Indonesia, the Lucena West 1 Colour Guard Team was among the best at the
+                        Tangerang Open Marching Competition.</p>
+                    <a href="https://www.facebook.com/share/p/19ATnq6H2g/" target="_blank">Read more</a>
+                </div>
+            </article>
+            <article class="news-article">
+                <img src="img/news2.jpg" alt="news2">
+                <div class="news-content">
+                    <h3 class="news-title">
+                        <span class="title-text">Bb. Pasayahan 2025</span>
+                        <span class="date">| May 26, 2025</span>
+                    </h3>
+                    <p class="summary">Kinoronahan bilang Binibining Pasayahan 2025 si Rueil Viana Montserrat ng Barangay
+                        Ibabang Dupay!
+                    </p>
+                    <a href="https://www.facebook.com/share/p/16da2rfooj/" target="_blank">Read more</a>
+                </div>
+            </article>
+            <article class="news-article">
+                <img src="img/news3.jpg" alt="news3">
+                <div class="news-content">
+                    <h3 class="news-title">
+                        <span class="title-text">Tagumpay ng Quezonian</span>
+                        <span class="date">| May 25, 2025</span>
+                    </h3>
+                    <p class="summary">Isang malaking karangalan ang hatid ng mga gurong Quezonian at mga unibersidad sa
+                        lalawigan ng Quezon matapos silang mapabilang sa hanay ng mga topnotchers at top performing schools sa
+                        nakalipas na March 2025 Licensure Examination for Professional Teachers</p>
+                    <a href="https://www.facebook.com/share/p/1C4tHW8229/" target="_blank">Read more</a>
+                </div>
+            </article>
+            <article class="news-article">
+                <img src="img/news4.jpg" alt="news4">
+                <div class="news-content">
+                    <h3 class="news-title">
+                        <span class="title-text">Summer Job Fair</span>
+                        <span class="date">| May 22, 2025</span>
+                    </h3>
+                    <p class="summary">Umarangkada ang kauna unahang Summer Job Fair ngayong taon ng PESO Lucena sa 3rd Floor
+                        Event Center ng SM City Lucena, May 22. Pinangunahan ang programa nina City Administrator Anacleto "Jun"
+                        Alcala Jr., na siyang kumatawan kay Mayor Kuya Mark Alcala at PESO Lucena OIC Acar Nacorda.</p>
+                    <a href="https://www.facebook.com/share/p/191xnFazNR/" target="_blank">Read more</a>
+                </div>
+            </article>
+
+            <article class="news-article">
+                <img src="img/news5.jpg" alt="news3">
+                <div class="news-content">
+                    <h3 class="news-title">
+                        <span class="title-text">Veterinary Medical Mission</span>
+                        <span class="date">| May 15, 2025</span>
+                    </h3>
+                    <p class="summary">Nitong Mayo 15, 2025, sa kahilingan ng St. Peter II (Pag-ibig) Subdivision Homeowner's
+                        Association, Inc. na pinangunahan ng kanilang presidente na si G. Ben N. Gedrano, ay nagsagawa ng
+                        Veterinary Medical Mission ang Office of the Provincial Veterinarian sa pamamagitan ni Dr. Camille
+                        Calaycay kasama ang mga technical personnel ng tanggapan. Ang naturang aktibidad ay ginanap sa Pag-ibig
+                        Covered Court, Brgy. Gulang Gulang, Lucena City.
+
+                    </p>
+                    <a href="https://www.facebook.com/share/p/1MNHEfENkZ/" target="_blank">Read more</a>
+                </div>
+            </article>
+
+            <article class="news-article">
+                <img src="img/news6.jpg" alt="news1">
+                <div class="news-content">
+                    <h3 class="news-title">
+                        <span class="title-text">OPLAN BAKLAS</span>
+                        <span class="date">| May 14, 2025</span>
+                    </h3>
+                    <p class="summary">Lucena City with its 33 barangays commenced the conduct of OPLAN BAKLAS pursuant to
+                        existing election laws like the Omnibus Election Code and Fair Election Act.</p>
+                    <a href="https://www.facebook.com/share/p/14Htek16Nco/" target="_blank">Read
+                        more</a>
+                </div>
+            </article>
+
+
+        </div>
+        <a href="#" id="loadMoreLink" class="load-more-link">Load More</a>
+    </section>
+
+    <div class="right-container">
+        <div id="announcement_section">
+            <h2>ANNOUNCEMENTS</h2>
+            <hr>
+
+            <div class="announcement-slider-wrapper">
+                <button class="arrow arrow-right" id="nextBtn" aria-label="Next slide">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="42" viewBox="0 0 30 42" fill="none"
+                        aria-hidden="true">
+                        <path d="M18.75 31.5L11.25 21L18.75 10.5" stroke="#B3B3B3" stroke-width="4" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </button>
+
+                <div class="announcement-slider">
+                    <div class="slides-container">
+                    </div>
+                </div>
+
+                <button class="arrow arrow-left" id="prevBtn" aria-label="Previous slide">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="42" viewBox="0 0 30 42" fill="none"
+                        aria-hidden="true">
+                        <path d="M11.25 31.5L18.75 21L11.25 10.5" stroke="#B3B3B3" stroke-width="4" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </button>
+            </div>
+
+            <div class="dots" role="tablist" aria-label="Slide navigation">
+            </div>
+        </div>
+
+
+
+        <div id="events_section">
+            <h2>EVENTS</h2>
+            <hr />
+            <div class="events_scroll-wrapper">
+                <div class="events_schedule">
+                    <span class="event-title">CHAMI LUCENA FESTIVAL</span>
+                    <span class="event-date">| May 22</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">FOAM RAVE PARTY</span>
+                    <span class="event-date">| May 22</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">LGBTQ+ GOT TALENT</span>
+                    <span class="event-date">| May 23</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">STREET FASHION SHOW</span>
+                    <span class="event-date">| May 24</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">FLORES DE MAYO</span>
+                    <span class="event-date">| May 25</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">Bb. PASAYAHAN 2025</span>
+                    <span class="event-date">| May 26</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">PEOPLE'S NIGHT</span>
+                    <span class="event-date">| May 27</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">GRAND PARADE</span>
+                    <span class="event-date">| May 28</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">HANDOG KAY SAN FERNANDO</span>
+                    <span class="event-date">| May 29</span>
+                </div>
+                <div class="events_schedule">
+                    <span class="event-title">SMB NIGHT</span>
+                    <span class="event-date">| May 29-31</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="news_script.js"></script>
+<script src="Navigation Bar/navigation.js"></script>
+
+<?php
+include 'Footer/Footer.php';
+?>
