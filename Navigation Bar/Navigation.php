3bebbfaa43b1960fc5e1d1b272234fc75b1b7f2b
noelztorres <noelztorresjr@gmail.com>
2025-06-23 18:58:54 +0800

    Add files via upload

---
+++ Navigation Bar/Navigation.php
@@ -0,0 +1,29 @@
+<div id="divNavigator">
+            <nav>
+                <ul class="sidebar" id="sidebar">
+                    <li onclick=hideSidebar()><a href="#"><i class="fas fa-times"></i></a></li>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="../About Page/aboutpage.html">About Lucena</a></li>
+                    <li><a href="../Services/services.html">Services</a></li>
+                    <li><a href="../News & Updates/news_updates.html">News & Updates</a></li>
+                    <li><a href="#">Tourism</a></li>
+                    <li><a href="#">Page Trasparency</a></li>
+                    <li><a href="../Contact Page/Contact Page.html">Contacts Us</a></li>
+                </ul>
+                <ul>
+                    <li class="hideOnDesktop" onclick=showSidebar()><a href="#"><i class="fas fa-bars"></i></a></li>
+                    <li class="hideOnMobile"><a href="../index.html">Home</a></li>
+                    <li class="hideOnMobile"><a href="../About Page/aboutpage.html">About Lucena</a></li>
+                    <li class="hideOnMobile"><a href="../Services/services.html">Services</a></li>
+                    <li class="hideOnMobile"><a href="../News & Updates/news_updates.html">News & Updates</a></li>
+                    <li class="hideOnMobile"><a href="#">Tourism</a></li>
+                    <li class="hideOnMobile"><a href="#">Page Trasparency</a></li>
+                    <li class="hideOnMobile"><a href="../Contact Page/Contact Page.php">Contacts Us</a></li>
+                </ul>
+            </nav>
+
+            <div id="divSearch">
+                <input type="search" id="search" name="fname" value="" placeholder="Search...">  
+                <button><i class="fas fa-search"></i></button>
+            </div>
+        </div>
